ebcf38f7b1926407db1c4f54dac89d1d2787aa3f
Allan Ebdrup <aeb@e-conomic.com>
2014-03-28 14:47:07 +0100

    router and pubsub store

--- brown/store.js
+++ brown/store.js
@@ -1,8 +1,11 @@
 var zmq = require('zmq')
 var address = 'tcp://*:30000';
 var pubAddress = 'tcp://*:30001';
+var routerAddress = 'tcp://*:30002';
 var socket = zmq.socket('dealer');
 var pubSocket = zmq.socket('pub');
+var routerSocket = zmq.socket('router');
+var clients = {};
 
 socket.bind(address, function(err) {
 	if (err){
@@ -12,9 +15,26 @@ socket.bind(address, function(err) {
 		if (err){
 			throw err;
 		}
-		socket.on('message', function (channel, content) {
-			console.log(new Date(), channel.toString(), content.toString());
-			pubSocket.send([channel, content]);
+		routerSocket.bind(routerAddress, function(err) {
+			if (err) {
+				throw err;
+			}
+			routerSocket.on('message', function(envelope, data) {
+				console.log(socket.identity + ': received ' + envelope.toString() + ' - ' + data.toString());
+				clients[data.toString()] = envelope;
+			});
+
+			socket.on('message', function (channel, content) {
+				console.log(new Date(), channel.toString(), content.toString());
+				pubSocket.send([channel, content]);
+				Object.keys(clients).forEach(function(channelToListenTo){
+					if(channel.toString().indexOf(channelToListenTo) === 0) {
+						var envelope = clients[channelToListenTo];
+						console.log('send', envelope.toString(), channel.toString(), content.toString());
+						routerSocket.send([envelope, channel, content]);
+					}
+				});
+			});
 		});
 	});
 });
