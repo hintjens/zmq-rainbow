f593346f3e7e4a7f4a78f64dba36bdf1291142a7
Pieter Hintjens <ph@imatix.com>
2014-03-28 13:30:41 +0100

    Let user specify endpoints

--- black/subscriber.c
+++ black/subscriber.c
@@ -2,11 +2,14 @@
 
 #include <czmq.h>
 
-int main (void)
+int main (int argc, char *argv [])
 {
     zctx_t *ctx = zctx_new ();
     void *subscriber = zsocket_new (ctx, ZMQ_SUB);
-    zsocket_connect (subscriber, "tcp://localhost:30001");
+    char *endpoint = "localhost";
+    if (argc > 1)
+        endpoint = argv [1];
+    zsocket_connect (subscriber, "tcp://%s:30001", endpoint);
     zsocket_set_subscribe (subscriber, "");
     
     while (true) {
