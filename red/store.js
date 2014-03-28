78d3c46c5c5ef81f26c332af49e4956c9a66a783
Eduardo Sorribas <eduardo@sorribas.org>
2014-03-28 13:38:51 +0100

    Cleanup store.js

--- red/store.js
+++ red/store.js
@@ -2,28 +2,14 @@ var zmq = require('zmq');
 var dsock = zmq.socket('dealer');
 var psock = zmq.socket('pub');
 
-dsock.bind('tcp://127.0.0.1:30000', function(err) {
+dsock.bind('tcp://*:30000', function(err) {
 	if (err) console.log(err);
 });
 
-psock.bind('tcp://127.0.0.1:30001', function(err) {
+psock.bind('tcp://*:30001', function(err) {
 	if (err) console.log(err);
 });
-
-dsock.on('bind', function() {
-	console.log('Bound');
-});
-
-psock.on('bind', function() {
-	console.log('Bound');
-});
-
-
-setTimeout(function() {
-	psock.send('HIIIIII');
-},1000);
 
 dsock.on('message', function(msg) {
-	console.log(msg);
 	psock.send(msg);
 });
